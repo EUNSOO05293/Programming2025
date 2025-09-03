abb7952a5ebfb4ee1efe772639b8886a6f335fbc
User <User@DESKTOP-TJR2KUF>
2025-09-03 11:43:41 +0900

    push

---
+++ Myfirst/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, World!\n");
+	return 0;
+}
+
+void test1()
+{
+	int age = 20;
+	const double PI = 3.141592;
+	char myname[128] = "이은수";
+    int iarray[] = {1, 2, 3, 4};
+
+	age += 1;
+	double r = 2; //2m
+	double area = PI * r * r; // 반지름 r인 면적
+
+	if( (age > 10) || (area > 5.0) )
+	{
+
+	}
+	else  
+	{
+
+	}
+}
